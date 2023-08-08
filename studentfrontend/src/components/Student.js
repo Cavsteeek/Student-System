180b4898c93bbc8d3fd68c067b06156a80922a69
Ikemdinachi Uzochukwu <uzochukwuikemdinachi@gmail.com>
2023-08-08 03:05:09 +0100

    Final Commit

--- studentfrontend/src/components/Student.js
+++ studentfrontend/src/components/Student.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
-import { Container, Paper } from '@material-ui/core';
+import { Container, Paper, Button } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -14,16 +14,53 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Student() {
     const paperStyle = {padding: '50px 20px', width:  600, margin: "20px auto"}
-  const classes = useStyles();
+    const[name,setName] = useState('')
+    const[address,setAddress] = useState('')
+    const[students, setStudents] = useState([])
+    const classes = useStyles();
+    const handleClick=(e)=>{
+        e.preventDefault()
+        const student = {name, address}
+        console.log(student)
+        fetch("http://localhost:8080/student/add",{
+            method: "POST",
+            headers:{"Content-Type":"application/json"},
+            body:JSON.stringify(student)
+        }).then(()=>{
+            console.log("New Student Added") 
+        })
+    }
+    useEffect(()=>{
+        fetch("http://localhost:8080/student/getAll")
+        .then(res=>res.json())
+        .then((result)=>{
+            setStudents(result);
+        }
+      )},[])
 
   return (
-    <Container>
+    <Container> 
         <Paper elevation={3} style={paperStyle}> 
             <h1 style={{color:"#007BFF"}}><u>New Student</u></h1>
     <form className={classes.root} noValidate autoComplete="off">
-      <TextField id="outlined-basic" label="Student Name" variant="outlined" fullWidth />
-      <TextField id="outlined-basic" label="Student Address" variant="outlined" fullWidth/>
+      <TextField id="outlined-basic" label="Student Name" variant="outlined" fullWidth value={name}
+      onChange={(e)=>setName(e.target.value)} />
+      <TextField id="outlined-basic" label="Student Address" variant="outlined" fullWidth value={address}
+      onChange={(e)=>setAddress(e.target.value)}/>
+    <Button variant="contained" color="secondary" onClick={handleClick}>
+    Submit
+    </Button>
     </form>
+    </Paper>
+    <h1> Students</h1>
+    <Paper elevation={3} style={paperStyle}>
+        {students.map(student=>(
+            <Paper elevation={6} style={{margin: "10px", padding:"15px", textAlign:"left"}} key={student.id}>
+                Id:{student.id}<br/>
+                Name:{student.name}<br/>
+                Address:{student.address}
+            </Paper>
+        ))}
     </Paper>
     </Container>  
   );
